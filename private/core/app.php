30a3983a9e6957dff7649c574d370f8fa0d7c8ef
AlexxBelous <connectionwithme1@gmail.com>
2023-08-17 07:58:33 +0300

    add part code

--- private/core/app.php
+++ private/core/app.php
@@ -13,12 +13,36 @@ class App
     public function __construct()
     {
         $URL = $this->getURL();
-        if (file_exists("../private/controllers/" . $URL[0] . ".php")) {
-            $this->controller = $URL[0];
+        $controllerFile = "../private/controllers/" . ucfirst($URL[0]) . ".php";
+
+        if (file_exists($controllerFile)) {
+            $this->controller = ucfirst($URL[0]);
+            unset($URL[0]);
+        } else {
+            // Попробуйте включить маленькую букву в имени файла
+            $controllerFile = "../private/controllers/" . strtolower($URL[0]) . ".php";
+
+            if (file_exists($controllerFile)) {
+                $this->controller = ucfirst(strtolower($URL[0]));
+                unset($URL[0]);
+            }
         }
-        require "../private/controllers/" . $this->controller . ".php";
+
+        require $controllerFile;
         $this->controller = new $this->controller();
+
+        if (isset($URL[1])) {
+            if (method_exists($this->controller, $URL[1])) {
+                $this->method = ucfirst($URL[1]);
+                unset($URL[1]);
+            }
+        }
+
+        $URL = array_values($URL);
+        $this->params = $URL;
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
+
 
     private function getURL()
     {
